07f9e393836293b032e8691e2507d576de5cda87
howardhhm <howardhhm@126.com>
2017-04-30 16:33:58 +0800

    update to the lastest kernel

--- init_linux.sh
+++ init_linux.sh
@@ -310,7 +310,7 @@ if [ "$HHM_UBUNTU_INIT_CLIENT" = "1" ]; then
     ## optional, but recommended
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
     ## add Typora's repository
-    add-apt-repository 'deb https://typora.io ./linux/'
+    add-apt-repository -y 'deb https://typora.io ./linux/'
 
     add-apt-repository -y ppa:nilarimogard/webupd8
     ## codeblocks
--- remove_cache.sh
+++ remove_cache.sh
@@ -4,31 +4,57 @@
 # @DateTime:     2017-02-22 11:29:07
 # @Description:  Description
 
+
+## update kernel
+sudo apt-get update
+echo
+echo "+++++++++++++++ alternative kernels +++++++++++++++"
+export ALL=$(apt-cache search linux-image | grep -v lowlatency \
+    | grep "Linux kernel image for version" | grep "generic" \
+    | awk '{print $1}' | sed 's/-generic//g;s/linux-image-//g' \
+    | sort -rV | head -n10)
+for i in $ALL
+do
+    echo $i
+done
+echo "+++++++++++++++ kernels end +++++++++++++++"
+echo
+echo "+++++++++++++++ LATEST kernels start +++++++++++++++"
+export LATEST=$(apt-cache search linux-image | grep -v lowlatency \
+    | grep "Linux kernel image for version" | grep "generic" \
+    | awk '{print $1}' | sed 's/-generic//g;s/linux-image-//g' \
+    | sort -rV | head -n1)
+echo $LATEST
+echo "+++++++++++++++ LATEST kernels end +++++++++++++++"
+echo
+sudo apt-get install linux-image-$LATEST linux-headers-$LATEST \
+    linux-headers-$LATEST-generic
+sudo apt-get install linux-image-$LATEST-lowlatency \
+    linux-headers-$LATEST-lowlatency
+
 ## remove system cache
 sudo apt-get autoclean
-sudo apt-get autoremove
+sudo apt-get -y autoremove
 ## remove old images
 echo
-echo "+++++++++++++++ all images start +++++++++++++++"
+echo "+++++++++++++++ all kernels start +++++++++++++++"
 dpkg --get-selections | grep linux
-echo "+++++++++++++++ all images end +++++++++++++++"
+echo "+++++++++++++++ all kernels end +++++++++++++++"
 echo
-export ALL=$(dpkg --get-selections | grep -v linux-image-extra \
-| grep -e "linux-image-[0-9]"| cut -f1 | sed "s|-generic||g;s|"\
-"linux-image-||g" | sort -rV)
-export LATEST=$(dpkg --get-selections | grep -v linux-image-extra \
-| grep -e "linux-image-[0-9]"| cut -f1 | sed "s|-generic||g;s|"\
-"linux-image-||g" | sort -rV | head -n 1)
-echo "+++++++++++++++ LATEST images start +++++++++++++++"
-for i in $LATEST
-do
-    dpkg --get-selections | grep linux | grep $i
-done
-echo "+++++++++++++++ LATEST images end +++++++++++++++"
+export ALL=$(dpkg --get-selections | cut -f1 | grep -e "linux-image-[0-9]" \
+| sed 's/-generic//g;s/linux-image-//g;s/-lowlatency//g' | sort -urV)
+export LATEST=$(dpkg --get-selections | cut -f1 | grep -e "linux-image-[0-9]" \
+| sed 's/-generic//g;s/linux-image-//g;s/-lowlatency//g' | sort -urV \
+| head -n 1)
+echo "+++++++++++++++ LATEST kernels start +++++++++++++++"
+
+# display the latest kernel
+dpkg --get-selections | grep linux | grep $LATEST
+
+echo "+++++++++++++++ LATEST kernels end +++++++++++++++"
 echo
-export OLD=$(dpkg --get-selections | grep -v linux-image-extra \
-| grep -e "linux-image-[0-9]"| cut -f1 | sed "s|-generic||g;s|"\
-"linux-image-||g" | grep -v $LATEST)
+export OLD=$(dpkg --get-selections | cut -f1 | grep -e "linux-image-[0-9]" \
+| sed 's/-generic//g;s/linux-image-//g;s/-lowlatency//g' | grep -v $LATEST)
 for i in $OLD
 do
     sudo apt-get purge $(dpkg --get-selections | grep linux \
